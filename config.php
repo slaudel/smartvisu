2589089dbd7a1ad17f65302380b6a3abf25db546
Sven Laudel <sven@laudel.net>
2016-10-16 10:56:25 +0200

    template kopiert, .gitignore

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+  * -----------------------------------------------------------------------------
+  * @package     smartVISU
+  * @author      Martin Gleiß
+  * @copyright   2012 - 2015
+  * @license     GPL [http://www.gnu.de]
+  * -----------------------------------------------------------------------------
+  */
+
+
+	define('config_design', 'night');
+	define('config_cache', false);
+	define('config_animation', false);
+	define('config_title', 'YOUR NAME [smartVISU]');
+	define('config_lang', 'en');
+	define('config_driver', 'fhem');
+	define('config_driver_address', '192.168.111.10');
+	define('config_driver_port', '2121');
+	define('config_driver_realtime', true);
+	define('config_driver_autoreconnect', true);
+	define('config_weather_service', 'offline');
+	define('config_weather_location', 'Germany/Bayern/Würzburg');
+	define('config_weather_key', '');
+	define('config_phone_service', 'offline');
+	define('config_phone_server', '192.168.x.x');
+	define('config_phone_port', '');
+	define('config_phone_user', '');
+	define('config_phone_pass', '');
+	define('config_calendar_service', 'offline');
+	define('config_calendar_url', 'http://www.google.com/calendar/feeds/...');
+	define('config_calendar_username', '');
+	define('config_calendar_password', '');
+	define('config_calendar_name', '');
+	define('config_proxy', false);
+	define('config_proxy_url', '');
+	define('config_proxy_user', '');
+	define('config_proxy_password', '');
+	define('config_js', 'min.js');
+
+?>
